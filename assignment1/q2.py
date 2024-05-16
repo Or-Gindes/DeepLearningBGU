1076df034df8e4aa324f5d8b81e28167bcf6acbd
RZdataprojects <egdv12@gmail.com>
2024-05-16 12:08:50 +0300

    change to np.sum

--- assignment1/q2.py
+++ assignment1/q2.py
@@ -19,7 +19,7 @@ def linear_backward(dZ: np.ndarray, cache: Tuple) -> Tuple:
     A_prev, W, b = cache
     m = A_prev.shape[1]
     dW = 1 / m * np.dot(dZ, A_prev.T)
-    db = 1 / m * dZ.sum(axis=1, keepdims=True)
+    db = 1 / m * np.sum(dZ, axis=1, keepdims=True)
     dA_prev = np.dot(W.T, dZ)
     return dA_prev, dW, db
 
