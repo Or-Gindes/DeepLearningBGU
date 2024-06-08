2b9a9f1f9a3624dfec018af66158b4804cc23cd4
RZdataprojects <egdv12@gmail.com>
2024-06-08 16:42:39 +0300

    add graphs

--- assignment2/q3.py
+++ assignment2/q3.py
@@ -7,7 +7,7 @@ from earlyStopping import EarlyStopping
 from prepareDataset import PrepareDataset
 from torchDataloader import FacesDataLoader
 from siameseNetwork import SiameseNetwork
-
+import matplotlib.pyplot as plt
 
 DATASET_FOLDER = "lfw2"
 LEARNING_RATE = 1e-4
@@ -34,14 +34,6 @@ def main():
         transform=transforms.Compose([transforms.Resize((105, 105)), transforms.ToTensor()])
     )
 
-    test_image_pairs, test_labels = ds.load_dataset(file_path=os.path.join(os.getcwd(), "pairsDevTest.txt"), mode='test')
-
-    validation_dataset = FacesDataLoader(
-        images=validation_image_pairs,
-        labels=validation_labels,
-        transform=transforms.Compose([transforms.Resize((105, 105)), transforms.ToTensor()])
-    )
-
     validation_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
     model = SiameseNetwork()
@@ -50,7 +42,7 @@ def main():
     lambda_ = lambda epoch: LAMBDA
     scheduler = lr_scheduler.MultiplicativeLR(optimizer, lr_lambda=lambda_)
     early_stopping = EarlyStopping(patience=PATIENCE)
-    model.train_model(
+    history = model.train_model(
         train_dataloader=train_dataloader,
         validation_dataloader=validation_dataloader,
         epoch=EPOCHS,
@@ -59,19 +51,38 @@ def main():
         scheduler=scheduler,
         early_stopping=early_stopping
     )
-    pass
 
-    # transform = transforms.Compose([transforms.Resize((105, 105)), transforms.ToTensor()])
-    # folder_dataset = datasets.ImageFolder(root='path_to_your_data')
-    # siamese_dataset = SiameseDataset(imageFolderDataset=folder_dataset, transform=transform)
-    # train_dataloader = DataLoader(siamese_dataset, shuffle=True, num_workers=8, batch_size=32)
-    #
-    # model = SiameseNetwork().to(device)
-    # criterion = ContrastiveLoss()
-    # optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    #
-    # train(model, train_dataloader, criterion, optimizer
+    # Plot train and validation losses
+    plt.figure(figsize=(10, 5))
+    plt.plot(history["train loss"], label="Train Loss")
+    plt.plot(history["validation loss"], label="Validation Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.title("Train and Validation Loss")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
+    # Plot train and validation ROC-AUC scores
+    plt.figure(figsize=(10, 5))
+    plt.plot(history["train ROC-AUC"], label="Train ROC-AUC")
+    plt.plot(history["validation ROC-AUC"], label="Validation ROC-AUC")
+    plt.xlabel("Epochs")
+    plt.ylabel("ROC-AUC Score")
+    plt.title("Train and Validation ROC-AUC")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    test_image_pairs, test_labels = ds.load_dataset(file_path=os.path.join(os.getcwd(), "pairsDevTest.txt"), mode='test')
+    test_dataset = FacesDataLoader(
+        images=test_image_pairs,
+        labels=test_labels,
+        transform=transforms.Compose([transforms.Resize((105, 105)), transforms.ToTensor()])
+    )
+    test_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    test_loss, test_auc = model.evaluate_model(test_dataloader)
+    print(f'Testset Loss: {test_loss:.3f}, ROC-AUC: {test_auc:.3f}')
 
 if __name__ == "__main__":
     main()
